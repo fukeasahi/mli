1ba01cb5d5ff21b9d71eee0c9a6b69e735ab1423
EC2 Default User <ec2-user@ip-172-31-84-33.ec2.internal>
2021-03-21 04:09:30 +0000

    config/routes.rb edit

--- config/routes.rb
+++ config/routes.rb
@@ -15,21 +15,5 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'public/homes#top'
-  get '/about', to: 'public/homes#about'
+  get '/about' => 'public/homes#about'
 end
-
-# new_customer_session GET    /customers/sign_in(.:format)                                                             plulic/sessions#new
-#             customer_session POST   /customers/sign_in(.:format)                                                             plulic/sessions#create
-#     destroy_customer_session DELETE /customers/sign_out(.:format)                                                            plulic/sessions#destroy
-#       new_customer_password GET    /customers/password/new(.:format)                                                        devise/passwords#new
-#       edit_customer_password GET    /customers/password/edit(.:format)                                                       devise/passwords#edit
-#           customer_password PATCH  /customers/password(.:format)                                                            devise/passwords#update
-#                             PUT    /customers/password(.:format)                                                            devise/passwords#update
-#                             POST   /customers/password(.:format)                                                            devise/passwords#create
-# cancel_customer_registration GET    /customers/cancel(.:format)                                                              devise/registrations#cancel
-#   new_customer_registration GET    /customers/sign_up(.:format)                                                             devise/registrations#new
-#   edit_customer_registration GET    /customers/edit(.:format)                                                                devise/registrations#edit
-#       customer_registration PATCH  /customers(.:format)                                                                     devise/registrations#update
-#                             PUT    /customers(.:format)                                                                     devise/registrations#update
-#                             DELETE /customers(.:format)                                                                     devise/registrations#destroy
-#                             POST   /customers(.:format)                                                                     devise/registrations#create
